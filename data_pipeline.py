d4d1ef879bc515e598fa7813ff8881bfb002fe45
rajasekb <rajasekhar.cv@gmail.com>
2021-12-05 12:06:57 +0530

    Hive Properties

--- data_pipeline.py
+++ data_pipeline.py
@@ -50,7 +50,7 @@ class Pipeline:
         return
 
     def create_spark_session(self):
-        self.spark = SparkSession.builder.appName("Hello_Fresh").enableHiveSupport().getOrCreate()
+        self.spark = SparkSession.builder.appName("Hello_Fresh").spark.conf.set("hive.exec.dynamic.partition.mode", "nonstrict").enableHiveSupport().getOrCreate()
 
 
 if __name__ == '__main__':
