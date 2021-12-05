5ee4c04ab194fb18a38ec71c3883b56eab309353
rajasekb <rajasekhar.cv@gmail.com>
2021-12-05 19:27:11 +0530

    Entire_data

--- transform.py
+++ transform.py
@@ -58,7 +58,12 @@ class Transform:
             df3 = self.spark.sql("select DIFFUCLTY,avg(Total_time) as Avg_Total_Time from (select a.Total_time,CASE WHEN a.Total_time <=30 THEN 'EASY' WHEN a.Total_time > 30 AND a.Total_time <=60 THEN 'MEDIUM' ELSE 'HARD' END AS DIFFUCLTY from (select (cookTimeMin+prepTimeTimeMin) as Total_time from Hello_fresh_Temp where (cookTimeMin+prepTimeTimeMin) >0 and upper(ingredients) like '%BEEF%') a ) b group by DIFFUCLTY")
             df4 = self.spark.sql("select diffuclty,avg_total_time from raj.Hello_Fresh_Final")
 
-            return df3
+            df3.createOrReplaceTempView("df3")
+            df4.createOrReplaceTempView("df4")
+
+            df5=self.spark.sql( "select a.DIFFUCLTY,(a.Avg_Total_Time+b.avg_total_time)/2 as Avg_Total_Time from df3 a join df4 b on a.DIFFUCLTY = b.DIFFUCLTY")
+
+            return df5
 
         except Exception as exp1:
             logging.error("An error while transforming daily data " + str(exp1))
